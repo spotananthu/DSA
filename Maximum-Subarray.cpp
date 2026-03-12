6ade757dcb0454a24cb2cc34916a93d269c46c72
spotananthu <ananthutemp@gmail.com>
2026-03-12 14:25:26 +0530

    [53] [Time Beats: 100.00%] [Memory Beats: 15.19%] - LeetPush

--- Maximum-Subarray.cpp
+++ Maximum-Subarray.cpp
@@ -2,20 +2,16 @@
 2public:
 3    int maxSubArray(vector<int>& nums) {
 4        
-5        long long sum = 0;
-6        long long maxi = LONG_MIN;
+5        double temp_maxVal =0;
+6        double maxVal = -DBL_MAX;
 7        for(int i=0;i<nums.size();++i)
 8        {
-9            sum+=nums[i];
-10
-11            if(sum > maxi)
-12                maxi = sum;
-13
-14            if(sum<0)
-15                sum = 0;
-16        }
-17
-18            return maxi;
-19    }
-20
-21};
+9
+10            temp_maxVal+=nums[i];
+11            maxVal = max(maxVal,temp_maxVal);
+12            if(temp_maxVal<0) temp_maxVal =0;
+13        }
+14
+15        return maxVal;
+16    }
+17};
